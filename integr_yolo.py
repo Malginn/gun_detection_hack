9ea67917930346ff5b9c924034e31085cbb900b7
Malginn <ivanov.na7069@gmail.com>
2023-10-26 19:10:14 +0500

    Update integr_yolo.py

--- integr_yolo.py
+++ integr_yolo.py
@@ -9,7 +9,7 @@ path = Path('/path/to/model.pt')
 model = YOLO(path)
 # from PIL
 im1 = Image.open("240292-Sepik.jpg")
-results = model.predict(source=im1, save=True)  # save plotted images in runs/detect/predict
+model.predict(source=im1, save=True, conf=0.1)  # save plotted images in runs/detect/predict
 
 # # для нескольких изображений
 # results = model.predict(source=[im1, im2])
